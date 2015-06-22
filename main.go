d989ef35e0e0522b4cee3a6bd296aa259dc32f27
setkeh <setkeh@gmail.com>
2015-06-23 00:54:07 +1000

    Fixed Json Decoder

--- main.go
+++ main.go
@@ -1,64 +1,85 @@
 package main
 
 import (
-	//"encoding/json"
+	"encoding/json"
 	"fmt"
-	"github.com/stretchr/objx"
-	"github.com/thoj/go-ircevent"
+	//"github.com/stretchr/objx"
+	//"github.com/thoj/go-ircevent"
 	"io/ioutil"
 	"math/rand"
 	//"os"
 )
 
-func request(conf string) (objx.Map, error) {
+type Config struct {
+	Confirc struct {
+		Nick     string
+		Trigger  string
+		Realname string
+		Server   string
+		Channels struct {
+			Ldc  string
+			Tskp string
+		}
+		AutoReconnect string
+		AutoRejoin    string
+		FloodDelay    string
+		Secure        string
+		Debug         string
+		Vhost         string
+	}
+	General struct {
+		Machine    string
+		Maintainer string
+	}
+	docker struct {
+		Host string
+		Port string
+	}
+	temp struct {
+		Temp       string
+		Pwm_duty   string
+		Start_temp string
+		Start_duty string
+	}
+}
+
+/*func AddCallbacks(conn *irc.Connection, config *Config) {
+	conn.AddCallback("001", func(e *irc.Event) {
+
+		fmt.Println(config)
+		//		var channels = config.Channels
+
+		//conn.Join(channels)
+	})
+}*/
+
+func main() {
+	var conf = "./config.json"
+	config := &Config{}
+	rand.Seed(64)
+
 	file, err := ioutil.ReadFile(conf)
 
 	if err != nil {
 		fmt.Println("Couldn't read config file, dying...")
-		//panic(err)
+		panic(err)
 		//os.Exit(1)
 	}
 
-	data, err := objx.FromJSON(string(file))
-
+	json.Unmarshal([]byte(file), &config)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 
-	return data, nil
-}
-
-func AddCallbacks(conn *irc.Connection, conf string) {
-	conn.AddCallback("001", func(e *irc.Event) {
-
-		config, err := request(conf)
-		if err != nil {
-			fmt.Println(err)
-		}
-		var channels = config.Get("confirc[0].channels[0]").Str()
-
-		conn.Join(channels)
-	})
-}
-
-func main() {
-	var conf = "./config.json"
-	rand.Seed(64)
-
-	config, err := request(conf)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
+	fmt.Println(config.Confirc.Nick)
 	//fmt.Sprintf(config.Get(confirc[0].channels).Str())
-	conn := irc.IRC(config.Get("confirc[0].nick").Str(), config.Get("confirc[0].realname").Str())
-	err = conn.Connect(config.Get("confirc[0].server").Str())
+	//	conn := irc.IRC(config.Get("confirc[0].nick").Str(), config.Get("confirc[0].realname").Str())
+	//	err = conn.Connect(config.Get("confirc[0].server").Str())
 
-	if err != nil {
-		fmt.Println("Failed to connect.", err)
-	}
+	//	if err != nil {
+	//		fmt.Println("Failed to connect.", err)
+	//	}
 
-	AddCallbacks(conn, conf)
-	conn.Loop()
+	//AddCallbacks( /*conn, */ config)
+	//conn.Loop()
 }
